d998f6a9fffdd1fff34d4ce765448c50c563cf67
ryiianqueiroz <ryanqueiroz2012@gmail.com>
2024-03-05 22:14:20 -0300

    html finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import { MainOverallComponent } from './components/main-overall/main-overall.component';
+import { NavComponent } from './components/nav/nav.component';
+import { OverviewSectionComponent } from './components/overview-section/overview-section.component';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavComponent, OverviewSectionComponent, MainOverallComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
